8c4810eaa08e6889e997a0304d11eb32cfdf4f06
Ahmed Salem <aieisalem17@gmail.com>
2023-07-05 09:18:00 +0100

    Add command to open a file in the default application

--- lisp/sys-utils.el
+++ lisp/sys-utils.el
@@ -14,5 +14,25 @@
      ((string-equal system-type "gnu/linux")
       (start-process "" nil "sakura" target-dir)))))
 
+
+(defun aieis/open-default (&optional file)
+  "Open a file with the default application"
+  (interactive)
+  (let ((target-file
+         (cond
+          ((string-equal major-mode "dired-mode") dired-directory)
+          (file file)
+          (buffer-file-name buffer-file-name)
+          (t nil))))
+
+    (when target-file
+      (cond
+       ((string-equal system-type "windows-nt")
+        (w32-shell-execute "open" target-file))
+       ((string-equal system-type "gnu/linux")
+        (start-process "" nil "xdg-open" target-file))))))
+
                                         ; Keybindings ;
+
 (define-key global-map (kbd "M-RET") 'aieis/open-terminal)
+(define-key global-map (kbd "M-S-<return>") 'aieis/open-default)
