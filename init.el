034a3a76667666ef731ea657e75c31477eaabcfe
Ahmed Salem <aieisalem17@gmail.com>
2023-08-21 11:53:25 +0100

    Remove menu and tool bars

--- init.el
+++ init.el
@@ -21,12 +21,7 @@
  '(org-agenda-files
    '("~/notes/tags.org" "~/notes/art.org" "~/notes/tasks.org" "~/notes/snippets.org"))
  '(warning-suppress-types '((emacs))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
 (put 'upcase-region 'disabled nil)
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
