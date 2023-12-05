82cff9e8f8c5adf267091390bac086587431ebdf
Ahmed Salem <aieisalem17@gmail.com>
2023-12-05 16:34:05 +0000

    Add new lisp modules to handel local setups / org / ue

---
+++ lisp/local.el
@@ -0,0 +1,30 @@
+;;; local.el ---  Local variables and configuration (os / work / home )
+;;; Commentary:
+
+;;; Code:
+;; Temporary Packages
+
+(unless (load "local-restrict" :noerror)
+  (write-region
+   ";;; local-restrict.el ---  Local variables and configuration (os / work / home )
+;;; Commentary:
+;;; Keep local / temporary data in here
+
+;;; Code:
+
+;; Temporary Packages
+(setq-default aieis/local-packages
+              (list))
+
+(provide 'local-restrict)
+;;; local-restrict.el ends here"
+   nil
+   (concat (file-name-directory (or load-file-name buffer-file-name)) "tmp.el")))
+
+(setq-default aieis/local-packages
+              (list 'ahk-mode))
+
+(provide 'local)
+;;; local.el ends here
+
+
