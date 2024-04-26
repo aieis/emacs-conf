6dcd9f5c2c28b16736ff93cfb6fbc555031cb16a
Ahmed Salem <aieisalem17@gmail.com>
2024-04-26 12:24:30 +0100

    Remove M-<Return> command due to conflict with vertico

--- lisp/sys-utils.el
+++ lisp/sys-utils.el
@@ -38,6 +38,5 @@
 
 ; Keybindings
 (define-key global-map (kbd "M-S-<return>") 'aieis/open-terminal)
-(define-key global-map (kbd "M-<return>") 'aieis/open-default)
 (provide 'sys-utils)
 ;;; sys-utils.el ends here
