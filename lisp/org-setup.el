100cc7f53265b22a3d820724eeee62e1738ca81e
Ahmed Salem <90198547+DHAhmedSalem@users.noreply.github.com>
2025-03-17 15:17:41 +0000

    Attempt to make org loading better

--- lisp/org-setup.el
+++ lisp/org-setup.el
@@ -1,21 +1,13 @@
 ;;; org-setup.el --- Setup org environment
 ;;; Commentary:
 ;;; Code:
-(require 'org)
-(require 'org-capture)
-(require 'org-roam)
 
 (with-eval-after-load 'org
-  (define-key global-map (kbd "C-c a") 'aieis/org-agenda)
-  (define-key global-map (kbd "C-c c") 'org-capture)
-
-  (custom-set-variables
-   '(org-return-follows-link t))
-  
+  (setq org-return-follows-link t)
   (let ((cmd-pairs
          '(("M-N" org-move-subtree-down)
            ("M-P" org-move-subtree-up))))
-    
+
     (mapc #'(lambda (pair) (define-key org-mode-map (kbd (car pair)) (cadr pair))) cmd-pairs))
 
   (setq org-capture-templates
@@ -39,14 +31,18 @@
     (message "Notes directory '%s' does not exist" notes-dir)))
 
 (with-eval-after-load 'org-roam
-  (define-key global-map (kbd "C-c n l") 'org-roam-buffer-toggle)
-  (define-key global-map (kbd "C-c n f") 'org-roam-node-find)
-  (define-key global-map (kbd "C-c n g") 'org-roam-graph)
-  (define-key global-map (kbd "C-c n i") 'org-roam-node-insert)
-  (define-key global-map (kbd "C-c n c") 'org-roam-capture)
-  (define-key global-map (kbd "C-c n j") 'org-roam-dailies-capture-today)
   (org-roam-db-autosync-mode)
   ;; If using org-roam-protocol
-  (require 'org-roam-protocol))
+  (require 'org-roam-protocol)))
+
+(define-key global-map (kbd "C-c a") 'aieis/org-agenda)
+(define-key global-map (kbd "C-c c") 'org-capture)
+
+(define-key global-map (kbd "C-c n l") 'org-roam-buffer-toggle)
+(define-key global-map (kbd "C-c n f") 'org-roam-node-find)
+(define-key global-map (kbd "C-c n g") 'org-roam-graph)
+(define-key global-map (kbd "C-c n i") 'org-roam-node-insert)
+(define-key global-map (kbd "C-c n c") 'org-roam-capture)
+(define-key global-map (kbd "C-c n j") 'org-roam-dailies-capture-today)
 (provide 'org-setup)
 ;;; org-setup.el ends here
